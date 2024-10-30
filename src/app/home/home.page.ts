cb18b3895ea2da46bc28caa5fdf5969d9d08806d
Pablo <pmacsal2707@g.educaand.es>
2024-10-30 14:51:43 +0100

    cosas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  operacion: string = '';
+  respuestaUsuario: number = 0;
+  resultado: string | undefined;
+  respuestaCorrecta: number | undefined;
 
-  constructor() {}
+  constructor() {
+    this.generarOperacion();
+  }
 
+  generarOperacion() {
+    const num1 = this.numAleatorio(1, 10);
+    const num2 = this.numAleatorio(1, 10);
+    const operador = this.numAleatorio(1, 4); // 1: suma, 2: resta, 3: multiplicación, 4: división
+
+    switch (operador) {
+      case 1:
+        this.operacion = `${num1} + ${num2}`;
+        this.respuestaCorrecta = num1 + num2;
+        break;
+      case 2:
+        this.operacion = `${num1} - ${num2}`;
+        this.respuestaCorrecta = num1 - num2;
+        break;
+      case 3:
+        this.operacion = `${num1} * ${num2}`;
+        this.respuestaCorrecta = num1 * num2;
+        break;
+      case 4:
+        this.operacion = `${num1} / ${num2}`;
+        this.respuestaCorrecta = num1 / num2;
+        break;
+    }
+  }
+
+  numAleatorio(a: number, b: number) {
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+  }
+
+  compruebaRespuesta() {
+    if (this.respuestaUsuario === this.respuestaCorrecta) {
+      this.resultado = '¡Correcto!';
+    } else {
+      this.resultado = 'Incorrecto. Intenta de nuevo.';
+    }
+  }
 }
